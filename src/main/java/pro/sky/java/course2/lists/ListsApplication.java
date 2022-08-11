429098161d02c76a1e9e4c885f33be43d5b2cd72
Stanislav <artemiev.stanislav@gmail.com>
2022-08-11 13:42:08 +0700

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/lists/ListsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.lists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListsApplication.class, args);
+	}
+
+}
